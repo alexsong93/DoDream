8c8cd57ddbb62581f3ef70f59e3825d27ca59e7b
Alex Song <ss456@duke.edu>
2013-05-16 00:45:23 -0400

    Initial move from local computer
    Home and About finished; currently working on Contact.

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$('body').css('display', 'none');
+	
+	$("body").fadeIn(750);
+
+	$('a').click(function(event){
+		event.preventDefault();
+		linkLocation = this.href;
+		$('body').fadeOut(750, redirectPage);
+	});
+
+	function redirectPage() {
+		window.location = linkLocation;
+	}
+
+	$('#menulist a').mouseenter(function(){
+		$(this).fadeTo('fast', 1);	
+	});
+	$('#menulist a').mouseleave(function(){
+		$(this).fadeTo('fast', 0.5);
+	});
+
+	$('.main img').mouseenter(function(){
+		$(this).fadeTo(200,0);
+	});
+	$('.main img').mouseleave(function(){
+		$(this).fadeTo(200,1);
+	});
+
+});
